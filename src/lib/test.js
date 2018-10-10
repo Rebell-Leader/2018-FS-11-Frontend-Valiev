dee6c1433055925a1c9450b8e37e165c44376fa6
rebekka-halal <vaa4207@gmail.com>
2018-10-11 02:25:17 +0500

    After 1st lecture

---
+++ src/lib/test.js
@@ -0,0 +1,8 @@
+//alert('Hello word');
+const say = function(name)
+{
+    return `Hello ${name}`;
+};
+
+export default say;
+//say - блок с фоном не дефолтным и выводить там текст сорбщения вместо алерта, читать доки по бабель и вебпак установить линтер
